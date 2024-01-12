0e39784462cf50556bf5a1ef64fdd0ba911d6777
Scott Main <scott@modular.com>
2024-01-12 09:38:48 -0800

    Fix function return

--- add.cpp
+++ add.cpp
@@ -4,4 +4,8 @@ torch::Tensor add(torch::Tensor input) {
   return input + 1;
 }
 
-static auto registry = torch::RegisterOperators("my_ops::add", add);
+// static auto registry = torch::RegisterOperators("my_ops::add", add);
+
+TORCH_LIBRARY(my_ops, m) {
+  m.def("add", &add);
+}
--- test_add.py
+++ test_add.py
@@ -7,7 +7,7 @@ print(torch.ops.my_ops.add(torch.ones((1, 10))))
 
 def add(input: torch.Tensor) -> torch.Tensor:
     """ This is needed for the onnx register_custom_op_symbolic call. """
-    torch.ops.my_ops.add(input)
+    return torch.ops.my_ops.add(input)
 
 class AddModule(nn.Module):
     def forward(self, input):
@@ -18,13 +18,10 @@ model = torch.nn.Sequential(
   AddModule(),  # WORKS FINE IF THIS LINE IS DELETED
   torch.nn.Linear(10, 10)
 )
-
 print(model)
 
 input = torch.ones((1, 10))
-print(type(input))
-
-torch.onnx.register_custom_op_symbolic("my_ops::add", add, 1)
+torch.onnx.register_custom_op_symbolic("my_ops::add", add, 16)
 torch.onnx.export(model, input, 'build/model.onnx', input_names=["input"], output_names=["output"])
 
 scripted_model = torch.jit.script(model)
